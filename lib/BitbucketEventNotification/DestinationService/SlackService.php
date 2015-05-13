f1e6cfb708964928961debdeabad096f430dabfd
Tetsuya Mori <tetsuya.mori@kidsstar.co.jp>
2015-05-13 15:57:19 +0900

    スペシャルメッセージ改良
    * と、絵文字ウザいので削除ｗ

--- lib/BitbucketEventNotification/DestinationService/SlackService.php
+++ lib/BitbucketEventNotification/DestinationService/SlackService.php
@@ -69,6 +69,7 @@ class SlackService extends DestinationService
         foreach ($user_name_dict as $bitbucket_name => $slack_name) {
             $message = preg_replace("!{$bitbucket_name}!", $slack_name, $message);
         }
+        $message = preg_replace('!\!\[[^]]+\]\(([^)]+)\)!', '\1', $message);
         if ($message) {
             $message .= "\n";
         }
@@ -86,17 +87,17 @@ class SlackService extends DestinationService
 
         $notify = '';
         if ($this->pullRequest instanceof PullRequestCreated) {
-            $notify .= sprintf("Pull request has been created by %s. Please review:bow:", $data['author']['display_name']);
+            $notify .= sprintf("Pull request has been created by %s. Please review.", $data['author']['display_name']);
             $notify .= sprintf("\nhttps://bitbucket.org/%s/pull-request/%d", $data['destination']['repository']['full_name'], $data['id']);
         } else if ($this->pullRequest instanceof PullRequestDeclined) {
-            $notify .= sprintf("Pull request has been declined by %s:disappointed:", $data['author']['display_name']);
+            $notify .= sprintf("Pull request has been declined by %s.", $data['author']['display_name']);
         } else if ($this->pullRequest instanceof PullRequestCommentCreated) {
-            $notify .= sprintf("Comment was posted by %s:star:", $data['user']['display_name']);
+            $notify .= sprintf("Comment was posted by %s.", $data['user']['display_name']);
             $notify .= sprintf("\n%s", PullRequest::replaceUrlForLink($data['links']['html']['href']));
         } else if ($this->pullRequest instanceof PullRequestMerged) {
-            $notify .= sprintf("Pull request has been merged by %s. Good job:sunglasses:", $data['author']['display_name']);
+            $notify .= sprintf("Pull request has been merged by %s. Good job.", $data['author']['display_name']);
         } else if ($this->pullRequest instanceof PullRequestUpdated) {
-            $notify .= sprintf("Pull request has been updated by %s. Please re-review:stuck_out_tongue:", $data['author']['display_name']);
+            $notify .= sprintf("Pull request has been updated by %s. Please re-review.", $data['author']['display_name']);
         } else {
             $notify = null;
         }
